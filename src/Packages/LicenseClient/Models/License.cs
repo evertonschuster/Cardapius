3e66c4fcf0f35c26bf7fb0dd8f570baa43f86577
Everton William Thoele Schuster <everton.schuster98@gmail.com>
2025-02-15 16:01:18 -0300

    Configurando pipelines para publicação de pacotes

---
+++ src/Packages/LicenseClient/Models/License.cs
@@ -0,0 +1,27 @@
+﻿namespace LicenseClient.Models
+{
+    public record License
+    {
+        public License(string key, DateTimeOffset expireAt)
+        {
+            Key = key;
+            ExpireAt = expireAt;
+        }
+
+        public bool IsValid
+        {
+            get => DateTimeOffset.Now < ExpireAt;
+
+        }
+
+        public DateTimeOffset ExpireAt { get; }
+
+        public string Key { get; }
+
+        internal static License Create(string key, DateTimeOffset expireAt)
+        {
+            return new License(key, expireAt);
+        }
+
+    }
+}
