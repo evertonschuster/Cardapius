208a49e2402979db6fa1f2cbb70af33e135a114f
Everton William Thoele Schuster <everton.schuster98@gmail.com>
2025-02-15 10:25:04 -0300

    Testando pipeline

---
+++ src/Packages/Packages/LicenseClient/ILicenseClient.cs
@@ -0,0 +1,10 @@
+﻿using LicenseClient.Models;
+
+namespace LicenseClient
+{
+    //https://chatgpt.com/c/67afa77c-82d8-8000-a3d8-2d2a180eac73
+    public interface ILicenseClient
+    {
+        License Get();
+    }
+}
---
+++ src/Packages/Packages/LicenseClient/LicenseClient.cs
@@ -0,0 +1,12 @@
+﻿using LicenseClient.Models;
+
+namespace LicenseClient
+{
+    class LicenseClient : ILicenseClient
+    {
+        public License Get()
+        {
+            return new License("ac3336d7-3a49-43b8-a1b9-4fef888aa8ea", DateTimeOffset.Now.AddDays(30));
+        }
+    }
+}
