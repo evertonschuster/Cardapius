33b0dcf338ad87bb2d3e384ab4f5a6d7c9c3595a
Everton William Thoele Schuster <everton.schuster98@gmail.com>
2025-08-04 19:39:41 -0300

    refactor: move login screen to shared auth

--- src/Fronts/erp/shared/auth/Login.test.tsx
+++ src/Fronts/erp/shared/auth/Login.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Login } from '../components/Login';
+import { Login } from './Login';
 
 describe('Login component', () => {
   test('renders required fields', () => {
