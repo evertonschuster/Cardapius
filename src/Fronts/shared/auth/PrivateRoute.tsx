4b7870d41bd46711e68154d1d91cf5a7440e62b6
Everton William Thoele Schuster <everton.schuster98@gmail.com>
2025-08-04 18:38:51 -0300

    feat(frontend): add jwt auth and offline cache

--- src/Fronts/shared/auth/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from './AuthContext';
-
-export const PrivateRoute = () => {
-  const { token } = useAuth();
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
-};
