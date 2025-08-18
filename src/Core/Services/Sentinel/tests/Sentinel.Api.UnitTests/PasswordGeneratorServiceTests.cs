30e4b995ba72adf58da501eb33542e047489517a
Everton William Thoele Schuster <everton.schuster98@gmail.com>
2025-08-17 21:30:35 -0300

    test: add user token service tests

---
+++ src/Core/Services/Sentinel/tests/Sentinel.Api.UnitTests/PasswordGeneratorServiceTests.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Sentinel.Api.Services;
+
+public class PasswordGeneratorServiceTests
+{
+    [Fact]
+    public void Generate_RespectsPasswordOptions()
+    {
+        var options = Options.Create(new PasswordOptions
+        {
+            RequiredLength = 10,
+            RequireDigit = true,
+            RequireLowercase = true,
+            RequireUppercase = true,
+            RequireNonAlphanumeric = true,
+            RequiredUniqueChars = 4
+        });
+
+        var service = new PasswordGeneratorService(options);
+        var password = service.Generate();
+
+        password.Length.Should().BeGreaterOrEqualTo(10);
+        password.Any(char.IsUpper).Should().BeTrue();
+        password.Any(char.IsLower).Should().BeTrue();
+        password.Any(char.IsDigit).Should().BeTrue();
+        password.Any(ch => "!@#$%^&*()-_=+[]{};:,.<>?".Contains(ch)).Should().BeTrue();
+        password.Distinct().Count().Should().BeGreaterOrEqualTo(4);
+    }
+
+    [Fact]
+    public void Generate_UsesMinLengthAndAllowedSymbols()
+    {
+        var options = Options.Create(new PasswordOptions
+        {
+            RequiredLength = 6,
+            RequireDigit = false,
+            RequireLowercase = false,
+            RequireUppercase = false,
+            RequireNonAlphanumeric = true
+        });
+
+        var service = new PasswordGeneratorService(options);
+        var password = service.Generate(minLength: 20, allowedSymbols: "*");
+
+        password.Length.Should().Be(20);
+        password.All(ch => ch == '*').Should().BeTrue();
+    }
+}
